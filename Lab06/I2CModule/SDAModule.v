4ea77fcc94c5ce5f42e71aa7cc7e997a8323f058
Kyle Daruwalla <daruwalla.kyle@gmail.com>
2014-10-26 17:41:19 -0400

    Updated SDA module.

--- Lab06/I2CModule/SDAModule.v
+++ Lab06/I2CModule/SDAModule.v
@@ -1,25 +1,45 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    10:16:58 10/21/2014 
-// Design Name: 
-// Module Name:    SDAModule 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// CM Box: 			 1608 & 1876
+// Engineer:		 Kyle Daruwalla & David McNeil
 //
-// Dependencies: 
+// Create Date:    10/21/2014
+// Module Name:    SDAModule
+// Description:
 //
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
+// A I2C SDA module designed to generate start, stop, data, and ACK bits.
 //
 //////////////////////////////////////////////////////////////////////////////////
-module SDAModule(
-    );
+module SDAModule(Read, Select, ShiftOut, StartStopACK, ShiftIn, SDA);
+input Read, Select, ShiftOut, StartStopACK;
+output reg ShiftIn; 
+inout reg SDA;
 
+parameter	HI_Z = 1'bZ;
+
+parameter	READ = 2'b1x,
+			START_STOP_ACK = 2'b01,
+			DATA = 2'b00;
+
+always @(Read or Select or ShittOut or StartStopACK) begin
+	casex({Read, Select})
+	READ: begin
+		ShiftIn <= HI_Z;
+		SDA <= HI_Z;
+	end
+	START_STOP_ACK: begin
+		ShiftIn <= StartStopACK;
+		SDA <= StartStopACK;
+	end
+	DATA: begin
+		ShiftIn <= ShittOut;
+		SDA <= ShittOut;
+	end
+	default: begin
+		ShiftIn <= HI_Z;
+		SDA <= HI_Z;
+	end
+	endcase
+end
 
 endmodule

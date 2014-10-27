05a251af5ebbc3ae480e3ecf312efc7bcdbf86f3
Kyle Daruwalla <daruwalla.kyle@gmail.com>
2014-10-27 11:12:24 -0400

    Updated MasterDataUnit

--- Lab06/I2CModule/MasterDataUnit.v
+++ Lab06/I2CModule/MasterDataUnit.v
@@ -1,25 +1,27 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    10:18:09 10/21/2014 
-// Design Name: 
-// Module Name:    MasterDataUnit 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// CM Box: 			 1608 & 1876
+// Engineer:		 Kyle Daruwalla & David McNeil
 //
-// Dependencies: 
+// Create Date:    10/27/2014
+// Module Name:    SDAModule
+// Description:
 //
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
+// A I2C SDA module designed to generate start, stop, data, and ACK bits.
 //
 //////////////////////////////////////////////////////////////////////////////////
-module MasterDataUnit(
-    );
+module MasterDataUnit(BaudRate, CLKFreq, CLK, BaudEN, RESET, ACK, Start, Stop, Read, Select, SendData, Shift, Load, SCL, SDA, ReceivedData);
+input [19:0] BaudRate;
+input [29:0] CLKFreq;
+input [7:0] SendData;
+input CLK, BaudEN, RESET, ACK, Start, Stop, Read, Select, Shift, Load;
+output [7:0] ReceivedData;
+output SCL, SDA;
 
+wire ShiftIn, ShiftOut;
+
+BaudRateGenerator BaudRateUnit(.BaudRate(BaudRate), .ClockFreq(CLKFreq), .CLK(CLK), .EN(BaudEN), .RESET(RESET), .CLK_OUT(SCL));
+SDAModule SDAUnit(.Read(Read), .Select(Select), .ShiftOut(ShiftOut), .ShiftIn(ShiftIn), .SDA(SDA));
+ShiftRegister ShiftRegisterUnit(.DataIn(SendData), .CLK(CLK), .RESET(RESET), .ShiftCLK(SCL), .ShiftIn(ShiftIn), .Shift(Shift), .Load(Load), .DataOut(ReceivedData), .ShiftOut(ShiftOut));
 
 endmodule
